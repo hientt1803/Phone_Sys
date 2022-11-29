154ed4d094f9f5f778893c4c8dff97b92b1301e3
HienTran <HienTran@192.168.0.116>
2022-11-29 23:06:54 +0700

    update banhang

--- Phone System/src/phonesystem/edu/view/Main_Frame.java
+++ Phone System/src/phonesystem/edu/view/Main_Frame.java
@@ -25,7 +25,6 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
